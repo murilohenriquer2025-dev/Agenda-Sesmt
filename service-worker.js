5ba2e39484b2916ed182e592c9bd1ce1c03c50d9
murilohenriquer2025-dev <murilohenriquer2024@gmail.com>
2025-09-10 11:56:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,2 @@
-const CACHE = 'agenda-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/Agenda-Sesmt/', '/Agenda-Sesmt/manifest.json', '/Agenda-Sesmt/icon-192.png'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('fetch', event => {});

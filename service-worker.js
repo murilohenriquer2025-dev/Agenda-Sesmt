01c8040625564cbb1aea622e3b5bcc93dab7d426
murilohenriquer2025-dev <murilohenriquer2024@gmail.com>
2025-09-11 10:29:34 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,29 @@
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('fetch', event => {});
+const CACHE_NAME = 'agenda-sesmt-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/Agenda-Sesmt/icons/icon-192.png',
+  '/Agenda-Sesmt/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
